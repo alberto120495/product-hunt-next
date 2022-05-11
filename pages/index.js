5254bea5008e2fb5f776a3d44f8ed2e63a0b2c14
Alberto <albertopimentel04@gmail.com>
2022-05-11 16:59:03 -0500

    mostrando los productos en la pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import Layout from "../components/layout/Layout";
-
+import { db } from "../firebase/config";
+import { collection, orderBy, getDocs } from "@firebase/firestore";
+import DetallesProducto from "../components/DetallesProducto";
 export default function Home() {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const obtenerProductos = async () => {
+      const datos = await getDocs(
+        collection(db, "productos"),
+        orderBy("creado", "desc")
+      );
+      setProductos(datos.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+    };
+    obtenerProductos();
+  }, []);
+
   return (
     <div>
       <Layout pagina="Inicio">
-        <h1>Home</h1>
+        <div className="listado-productos">
+          <div className="contenedor">
+            <ul className="bg-white">
+              {productos.map((producto) => (
+                <DetallesProducto key={producto.id} producto={producto} />
+              ))}
+            </ul>
+          </div>
+        </div>
       </Layout>
     </div>
   );
